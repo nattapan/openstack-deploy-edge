3ac7a0c01b324f7380596ee3be2e3b031c4246d7
nattapan <3727102+nattapan@users.noreply.github.com>
2022-01-21 14:42:21 +0700

    Update install-openstack.sh

--- install-openstack.sh
+++ install-openstack.sh
@@ -1,7 +1,8 @@
 echo "need Super User Level before run this script"
 sudo apt update && sudo apt upgrade -y
-snap install microstack --devmode --edge
+sudo snap install microstack --devmode --edge
 
 # /var/snap/microstack/common/etc/horizon/local_settings.d/_05_snap_tweaks.py
-sed 's/DEBUG = False/DEBUG = True/g' /var/snap/microstack/common/etc/horizon/local_settings.d/_05_snap_tweaks.py
+sudo sed  -i 's/DEBUG = False/DEBUG = True/g' /var/snap/microstack/common/etc/horizon/local_settings.d/_05_snap_tweaks.py
 #
+echo "Micostack installed and enable WebGUI"
